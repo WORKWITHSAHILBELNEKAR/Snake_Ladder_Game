76d43ec0f99e329d554bb832971efc0559379b35
sahil belnekar <sahilsbelnekar2002@gmail.com>
2024-08-06 11:06:12 +0530

    added welcome task

---
+++ src/SnakeGameLadderGame/SnakeLadderGame.java
@@ -0,0 +1,7 @@
+package SnakeGameLadderGame;
+
+public class SnakeLadderGame {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Snake And Ladder game!");
+    }
+}
