228c3a9df6e0c55b947dba0c9568091a82717e7a
sahil belnekar <sahilsbelnekar2002@gmail.com>
2024-08-06 11:28:34 +0530

    start position added

--- src/SnakeGameLadderGame/SnakeLadderGame.java
+++ src/SnakeGameLadderGame/SnakeLadderGame.java
@@ -1,6 +1,7 @@
 package SnakeGameLadderGame;
 
 public class SnakeLadderGame {
+    static int Player1 = 0 ;  //
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake And Ladder game!");
     }
