82456d86974ab6636e035c5b40055546925db18c
sahil belnekar <sahilsbelnekar2002@gmail.com>
2024-08-06 11:51:48 +0530

    task 3 added

--- src/SnakeGameLadderGame/SnakeLadderGame.java
+++ src/SnakeGameLadderGame/SnakeLadderGame.java
@@ -1,8 +1,16 @@
 package SnakeGameLadderGame;
 
+import java.util.Random;
+
 public class SnakeLadderGame {
     static int Player1 = 0 ;  //
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake And Ladder game!");
     }
+
+    private static int rollDice(){
+        Random random = new Random();
+        return random.nextInt(6)+1;
+    }
+
 }
